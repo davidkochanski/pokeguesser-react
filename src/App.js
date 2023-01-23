5eac874c7f0eb9d7f444bf669d76214a54c41277
David Kochanski <105320855+davefoxxo@users.noreply.github.com>
2023-01-22 21:02:28 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Pokemon(props) {
+    return (
+        <img className="mon" src={"" + props.url} alt="mon" onClick={props.onClick}/>
+    )
 }
 
-export default App;
+
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dexNumber: this.getRandomDexNumber()
+        }
+        this.handleClick = this.handleClick.bind(this);
+        
+        
+    }
+
+    getRandomDexNumber() {
+        return Math.floor(Math.random() * 898) + 1
+    }
+
+    handleClick() {
+        console.log(this.state);
+    }
+
+
+    render() {
+        return(
+            <Pokemon url={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + this.state.dexNumber + ".png"}
+                     onClick={() => this.handleClick()}/>
+        );
+    }
+
+
+
+
+
+}
+
+
+
+
+export default Game;
