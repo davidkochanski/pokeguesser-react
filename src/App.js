5ce65e7bede765301edc53f745080a7fe7117060
David Kochanski <105320855+davefoxxo@users.noreply.github.com>
2023-01-22 21:46:34 -0500

    Add non-functional hint and input

--- src/App.js
+++ src/App.js
@@ -7,6 +7,57 @@ function Pokemon(props) {
     )
 }
 
+class Guesser extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            guess: ""
+        }
+    }
+
+    handleChange(event) {
+        this.setState({ guess: event.target.value });
+    };
+
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" value={this.state.guess} onChange={(event) => this.handleChange(event)}/>
+                </form>
+            <p>{this.state.guess}</p>
+            </div>
+        )
+
+    }
+}
+
+class HintButton extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            used: false,
+            text: "Hint!"
+        }
+        
+    }
+
+    handleClick(event) {
+        if(!this.state.used) {
+            this.setState( {used: true, text: this.props.theHint} )
+        }
+    }
+
+    render() {
+        return (
+            <button onClick={(e) => this.handleClick(e)}>
+                {this.state.text}
+            </button>
+        )
+    }
+}
 
 
 class Game extends React.Component {
@@ -31,14 +82,14 @@ class Game extends React.Component {
 
     render() {
         return(
-            <Pokemon url={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + this.state.dexNumber + ".png"}
-                     onClick={() => this.handleClick()}/>
+            <div>
+                <Pokemon url={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + this.state.dexNumber + ".png"}
+                        onClick={() => this.handleClick()}/>
+                <Guesser/>
+                <HintButton theHint={"This Pokemon's dex number is " + this.state.dexNumber}/>
+            </div>
         );
     }
-
-
-
-
 
 }
 
