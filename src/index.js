47f7adb4febc79bff7d9eb0cd9ce319be487e9dc
David Kochanski <105320855+davefoxxo@users.noreply.github.com>
2023-01-28 19:16:10 -0500

    Add hint and combo systems

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Game from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('game'));
 root.render(
   <React.StrictMode>
     <Game />
