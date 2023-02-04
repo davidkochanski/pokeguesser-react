02948f3d2349a5f3674d17a7f9c5ead8b00472e6
David Kochanski <105320855+davefoxxo@users.noreply.github.com>
2023-02-04 18:40:56 -0500

    Moving menu into own child class

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class Game extends React.Component {
         super(props);
         let dex = this.getRandomDexNumber()
 
+        // TODO Put timer in super class
         this.state = {
             dexNumber: dex,
             guess: "",
@@ -45,7 +46,8 @@ class Game extends React.Component {
             hintDisabled: false,
             lineText: this.getInitialLineText(dex),
             score: 0,
-            timeLeft: this.props.timeLeft,
+            // timeLeft: this.props.timeLeft,
+            timeLeft: this.props.initialTime,
             combo: 0,
             difficulty: this.props.difficultyText,
             monsSkipped: 0,
@@ -57,9 +59,8 @@ class Game extends React.Component {
         this.handleHintClick = this.handleHintClick.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.startTimer = this.startTimer.bind(this);
-        this.countDown = this.countDown.bind(this);
-        this.timer = 0;
+
+        // this.timer = 0;
         
     }
 
@@ -141,70 +142,99 @@ class Game extends React.Component {
     
     }
 
-    countDown() {
-        this.setState( {timeLeft: this.state.timeLeft - 1} );
+    // countDown() {
+    //     this.setState( {timeLeft: this.state.timeLeft - 1} );
         
-        if (this.state.timeLeft === 1) {
-            clearInterval(this.timer);
-        }
-    }
+    //     if (this.state.timeLeft === 1) {
+    //         clearInterval(this.timer);
+    //     }
+    // }
 
-    startTimer() {
-        if (this.timer === 0 && this.state.timeLeft > 0) {
-            this.timer = setInterval(this.countDown, 1000);
-          }
-    }
+    // startTimer() {
+    //     if (this.timer === 0 && this.state.timeLeft > 0) {
+    //         this.timer = setInterval(this.countDown, 1000);
+    //       }
+    // }
 
     displayTime() {
         if(this.props.timeText === "Zen Mode") {
             return <i className="fa-solid fa-infinity"></i>
         } else {
-            return this.state.timeLeft
+            return this.props.currentTime;
         }
     }
 
+    gameReset() {
+        
+
+    }
+
+    renderGameOver() {
+        if(this.state.timeLeft === 0) {
+            return (
+                <div className="modal">
+                    <div className="modal-content">
+                        <h2>Time's Up!</h2>
+                        <p>Score: {this.state.score}</p>
+                        <p>Mons Skipped: {this.state.monsSkipped}</p>
+                        <p>Hints Used: {this.state.hintsTaken}</p>
+
+
+                        <button onClick={() => this.gameReset()}>Play Again?</button>
+
+                    </div>
+                </div>
+            )
+        }
+    }
+
+
+
     render() {
-        this.startTimer();
 
         return (
-            <div className="game">
-                <div className="text-array">
-                    <h2>{"Score: " + this.state.score}</h2>
-                    <h2 className={(this.state.timeLeft <= 10 && this.state.timeLeft % 2 === 0 && this.props.timeText !== "Zen Mode") ? "red" : ""}>
-                        {this.displayTime()}
-                    </h2>
+                <div className="game">
+                    <div className="text-array">
+                        <h2>{"Score: " + this.state.score}</h2>
+                        <h2 className={(this.state.timeLeft <= 10 && this.state.timeLeft % 2 === 0 && this.props.timeText !== "Zen Mode") ? "red" : ""}>
+                            {this.displayTime()}
+                        </h2>
+                    </div>
+
+                    <Pokemon url={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + (this.state.dexNumber) + ".png"}
+                            onClick={() => this.newPokemon(true)}/>
+                            
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" value={this.state.guess} onChange={(event) => this.handleInputChange(event)}/>
+                    </form>
+                    <p className="linetext">{this.state.lineText}</p>
+                </div>
+                <div className="button-array">
+                    <button onClick={() => this.newPokemon(true)}>
+                        {"Pass"}
+                    </button>
+                    <button onClick={() => this.handleHintClick()}>
+                        {"Hint"}
+                    </button>
+                    
+                </div>
+                <h3>{this.state.difficulty}</h3>
+                <h3>{this.state.monsGuessed} | {this.state.monsSkipped} | {this.state.hintsTaken}</h3>
+                <h3>{this.state.hintMessage}</h3>
+                <p className="combo" key={this.state.combo}>{this.state.combo <= 1 ? null : this.state.combo + "x Combo!"}</p>
+
+
+                {this.renderGameOver()}
+
                 </div>
 
-                <Pokemon url={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + (this.state.dexNumber) + ".png"}
-                        onClick={() => this.newPokemon(true)}/>
-                        
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.guess} onChange={(event) => this.handleInputChange(event)}/>
-                </form>
-                <p className="linetext">{this.state.lineText}</p>
-            </div>
-            <div className="button-array">
-                <button onClick={() => this.newPokemon(true)}>
-                    {"Pass"}
-                </button>
-                <button onClick={() => this.handleHintClick()}>
-                    {"Hint"}
-                </button>
-                
-            </div>
-            <h3>{this.state.difficulty}</h3>
-            <h3>{this.state.monsGuessed} | {this.state.monsSkipped} | {this.state.hintsTaken}</h3>
-            <h3>{this.state.hintMessage}</h3>
-            <p className="combo" key={this.state.combo}>{this.state.combo <= 1 ? null : this.state.combo + "x Combo!"}</p>
-            </div>
         );
     }
 }
 
 
-
-class App extends React.Component {
+class Menu extends React.Component {
     constructor(props) {
         super(props);
 
@@ -216,53 +246,16 @@ class App extends React.Component {
         }
     }
 
-    handleStartClick() {
-        this.setState( {active: true} );
 
-    }
-
-    handleRangeChange(event) {
-        let e = event.target.value
-        if(e === "25") {
-            this.setState( {difficultyText: "Easy"} )
-        } else if(e === "75") {
-            this.setState( {difficultyText: "Normal"} )
-        } else {
-            this.setState( {difficultyText: "Hard"} )
-        }
-    }
-
-    handleTimeChange(event) {
-        let e = event.target.value
-        if(e === "25") {
-            this.setState( {time: 30, timeText: "30 sec."} )
-
-        } else if(e === "50") {
-            this.setState( {time: 60, timeText: "1 min"} )
-
-        } else if(e === "75") {
-            this.setState( {time: 180, timeText: "3 min"} )
-
-        } else if(e === "100") {
-            this.setState( {time: 300, timeText: "5 min"} )
-
-        } else if(e === "125") {
-            this.setState( {time: 0, timeText: "Zen Mode"} )
-        }
-
-    }
-
-
-    renderStartPage() {
-        if(this.state.active) return null
+    render() {
             
         return (
             <div className="start">
                 <form>
                     <div className="form-section">
                         <label htmlFor="difficulty">Difficulty</label>
-                        <input onChange={(event) => this.handleRangeChange(event)} type="range" id="difficulty" name="difficulty"  min="25" max="125" step="50"/>
-                        <h2>{this.state.difficultyText}</h2>
+                        <input onChange={(event) => this.props.difficultyChange(event)} type="range" id="difficulty" name="difficulty"  min="25" max="125" step="50"/>
+                        <h2>{this.props.difficultyText}</h2>
                         <i className="fa-solid fa-circle-info">
                             <span className="tooltip">
                                 <div className="tooltip-header"><strong>Easy Mode</strong></div>
@@ -279,30 +272,131 @@ class App extends React.Component {
 
                     <div className="form-section">
                         <label htmlFor="time">Time</label>
-                        <input onChange={(event) => this.handleTimeChange(event)} type="range" id="time" name="time"  min="25" max="125" step="25"/>
-                        <h2>{this.state.timeText}</h2>
-                        <i className="fa-solid ">
+                        <input onChange={(event) => this.props.timeChange(event)} type="range" id="time" name="time"  min="25" max="125" step="25"/>
+                        <h2>{this.props.timeText}</h2>
+                        <i className="fa-solid">
                             {/* pass */}
                         </i>
                     </div>
                 </form>
 
-                <button onClick={() => this.handleStartClick()}>
+                <button onClick={(event) => this.props.startButton(event)}>
                     Start
                 </button>
             </div>
         )
-        
+        }
+}
+
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleDifficultyChange = this.handleDifficultyChange.bind(this);
+        this.handleTimeChange = this.handleTimeChange.bind(this);
+        this.handleStartButton = this.handleStartButton.bind(this);
+        this.startTimer = this.startTimer.bind(this);
+        this.countDown = this.countDown.bind(this);
+
+        this.state = {
+            difficultyText: "Normal",
+            timeText: "1 min",
+            time: 60,
+            gameActive: false,
+
+
+        }
+
+        this.timer = 0;
+
     }
+
+
+    // renderGame() {
+    //     return this.state.active ? <Game difficulty={this.state.difficultyText} timeText={this.state.timeText} timeLeft={this.state.time}/> : null
+    // }
+
+    handleDifficultyChange(event) {
+        let e = event.target.value;
+        if(e === "25") {
+            this.setState( {difficultyText: "Easy"} )
+        } else if(e === "75") {
+            this.setState( {difficultyText: "Normal"} )
+        } else {
+            this.setState( {difficultyText: "Hard"} )
+        }
+    }
+
+    handleTimeChange(event) {
+        let e = event.target.value;
+        if(e === "25") {
+            this.setState( {time: 31, timeText: "30 sec"} )
+
+        } else if(e === "50") {
+            this.setState( {time: 62, timeText: "1 min"} )
+
+        } else if(e === "75") {
+            this.setState( {time: 181, timeText: "3 min"} )
+
+        } else if(e === "100") {
+            this.setState( {time: 301, timeText: "5 min"} )
+
+        } else if(e === "125") {
+            this.setState( {time: 999999999, timeText: "Zen Mode"} )
+        }
+    }
+
+    handleStartButton(event) {
+        this.setState( {gameActive: !this.state.gameActive} )
+    }
+
+
+    startTimer() {
+        if (this.timer === 0 && this.state.time > 0) {
+            this.timer = setInterval(this.countDown, 1000);
+          }
+    }
+
+
+    countDown() {
+        this.setState( {time: this.state.time - 1} );
+
+        if (this.state.time === 1) {
+            clearInterval(this.timer);
+        }
+    }
+
+    renderMenu() {
+        if(!this.state.gameActive) return (
+            <Menu 
+                timeChange={this.handleTimeChange}
+                timeText={this.state.timeText}
+                difficultyChange={this.handleDifficultyChange} 
+                difficultyText={this.state.difficultyText}
+                startButton={this.handleStartButton}
+            />
+        )
+    }
+
     renderGame() {
-        return this.state.active ? <Game difficulty={this.state.difficultyText} timeText={this.state.timeText} timeLeft={this.state.time}/> : null
+        if(this.state.gameActive) return (
+            <Game
+                initialTime={this.state.time}
+                difficulty={this.state.difficultyText}
+                currentTime={this.state.time}
+            />
+        )
     }
     
 
     render() {
+        this.startTimer();
+
         return (
             <div>
-                {this.renderStartPage()}
+                {this.renderMenu()}
                 {this.renderGame()}
             </div>
         )
